87c87ec9563609d402ccbc465d4cccaa42e81313
K. Arthur Endsley <endsley@umich.edu>
2023-10-14 13:06:18 -0600

    Minor change to comment in build.sh

--- docs/build.sh
+++ docs/build.sh
@@ -1,5 +1,5 @@
 # Use:
-#   sh build.sh <path_to_pyl4c_repo> <path_to_output_dir>
+#   sh build.sh <path_to_mod17_repo> <path_to_output_dir>
 
 pdoc --html -c latex_math=True --force -o $2 $1
 mv $2/mod17/*.html $2
